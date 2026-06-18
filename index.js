3e9f26f6125b09f65f38ddd4b4eeb0313dc630db
Hey-Ismail <Heythereitsfahim@gmail.com>
2026-06-18 20:43:56 +0600

    fetching data from database via server

--- index.js
+++ index.js
@@ -29,6 +29,30 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        //fetching data from the databsse*
+        const database = client.db("simpleCRUD")
+        const ideasCollection = database.collection("ideas")
+        app.get("/ideas", async (req, res) => {
+            const ideas = await ideasCollection.find().toArray();
+            res.send(ideas);
+        });
+
+
+        //showing every card in detailes
+        app.get("/ideas/:id", async (req, res) => {
+            const id = Number(req.params.id);
+
+            const result = await ideasCollection.findOne({
+                id: id,
+            });
+
+            res.send(result);
+        });
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
